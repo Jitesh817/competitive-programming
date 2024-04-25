6d4bed439438160d9dd45260ec5f5bfe643e36d8
Jitesh Yadav <ydv.jitesh10@gmail.com>
2024-04-25 23:24:07 +0530

    [topInterview150] practice session 25th April 2024

---
+++ cpp/leetcode/topInterview150/section-array-string/42-Trapping-Rain-Water.cpp
@@ -0,0 +1,36 @@
+/*
+
+https://leetcode.com/problems/trapping-rain-water
+
+*/
+
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+        int n = height.size();
+        vector<int> maxFromRight(n);
+
+        maxFromRight[n-1] = height[n-1];
+        for(int i = n-2; i >= 0; i--) {
+            maxFromRight[i] = maxFromRight[i+1] > height[i] ? maxFromRight[i+1] : height[i];
+        }
+
+        int trapped = 0;
+        int maxFromLeft = height[0];
+        for(int i = 1; i < n-1; i++) {
+            maxFromLeft = maxFromLeft > height[i] ? maxFromLeft : height[i];
+            trapped += min(maxFromLeft, maxFromRight[i]) - height[i];
+        }
+
+        return trapped;
+    }
+};
+
+int main() {
+    return 0;
+}
