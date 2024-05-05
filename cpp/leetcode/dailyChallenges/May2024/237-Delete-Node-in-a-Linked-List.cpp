e840f5d03a785254d0e26af489be358414ab90ee
Jitesh Yadav <ydv.jitesh10@gmail.com>
2024-05-05 11:15:53 +0530

    [dailyChallenge] 5th May 2024

---
+++ cpp/leetcode/dailyChallenges/May2024/237-Delete-Node-in-a-Linked-List.cpp
@@ -0,0 +1,43 @@
+/*
+
+https://leetcode.com/problems/delete-node-in-a-linked-list
+
+*/
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+int main() {
+    return 0;
+}
